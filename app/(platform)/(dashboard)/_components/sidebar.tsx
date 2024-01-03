a8437793008ceadcde37cc046845b53698b48520
Kimmi17 <dangthikimthuybl@gmail.com>
2024-01-03 13:54:09 +0200

    Add hooks to Sidebar component

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,8 +1,21 @@
 "use client";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+
 interface SidebarProps {
   storageKey?: string;
 }
 
-export const Sidebar = ({ storageKey }: SidebarProps) => {
-  return <div>Sidebar!</div>;
+export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
 };

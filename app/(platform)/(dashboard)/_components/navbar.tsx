a9f48bc88200f7449ce03c84245ca8e57b0d05b3
Kimmi17 <dangthikimthuybl@gmail.com>
2024-01-03 12:46:58 +0200

    Add Button to Navbar of Organization Page

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,4 +1,7 @@
 import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
 
 export const Navbar = () => {
   return (
@@ -7,6 +10,15 @@ export const Navbar = () => {
         <div className="hidden md:flex">
           <Logo />
         </div>
+        <Button
+          size="sm"
+          className="rounded-sm hidden md:block h-auto  py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button size="sm" className="rounded-sm block md:hidden">
+          <Plus className="h-4 w-4" />
+        </Button>
       </div>
     </nav>
   );

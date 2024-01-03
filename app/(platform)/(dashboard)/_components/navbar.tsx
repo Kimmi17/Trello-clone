f2b0b53005b85d4b82d1da3c5fd4cae43c499b8a
Kimmi17 <dangthikimthuybl@gmail.com>
2024-01-03 12:40:13 +0200

    Add styling, logo to Navbar of Organization Page

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,3 +1,13 @@
+import { Logo } from "@/components/logo";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+      </div>
+    </nav>
+  );
 };

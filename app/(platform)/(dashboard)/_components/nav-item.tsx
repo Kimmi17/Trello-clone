cf99845a360f8c8a114b16f414111d9a07c75c50
Kimmi17 <dangthikimthuybl@gmail.com>
2024-01-03 14:42:24 +0200

    Add AccordionItem, AccordionTrigger

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,4 +1,8 @@
 "use client";
+
+import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
+
 export type Organization = {
   id: string;
   slug: string;
@@ -17,5 +21,15 @@ export const NavItem = ({
   organization,
   onExpand,
 }: NavItemProps) => {
-  return <div>NavItem</div>;
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline",
+          isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+        )}
+      ></AccordionTrigger>
+    </AccordionItem>
+  );
 };

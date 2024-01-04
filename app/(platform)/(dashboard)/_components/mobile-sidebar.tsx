227b2466b855bba56fdad53f746e8c8b30e49cb6
Kimmi17 <dangthikimthuybl@gmail.com>
2024-01-04 07:54:22 +0200

    Add button to Mobile-sidebar

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Sidebar } from "./sidebar";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
 export const MobileSidebar = () => {
   const pathname = usePathname();
@@ -24,5 +28,21 @@ export const MobileSidebar = () => {
     return null;
   }
 
-  return <div>Mobile Sidebar!</div>;
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        className="block md:hidden mr-2"
+        variant="ghost"
+        size="sm"
+      >
+        <Menu className="h-4 w-4" />
+      </Button>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent side="left" className="p-2 pt-10">
+          <Sidebar storageKey="t-sidebar-mobile-state" />
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };

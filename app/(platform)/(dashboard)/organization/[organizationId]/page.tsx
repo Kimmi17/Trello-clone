ad520f712c32f810402cfed4714746d4c8a54e21
Kimmi17 <dangthikimthuybl@gmail.com>
2024-01-04 09:59:21 +0200

    Add form and display on OrganizationIdPage

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,28 @@
-import { OrganizationSwitcher } from "@clerk/nextjs";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-const OrganizationPage = () => {
-  return <div>Organization Page !</div>;
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board title: {board.title} </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default OrganizationPage;
+export default OrganizationIdPage;
